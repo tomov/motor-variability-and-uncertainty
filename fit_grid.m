a67811941f91b0c936b8fac3e9ef0487eae35b4f
Momchil Tomov <tomov90@gmail.com>
2019-02-27 08:29:14 -0500

    grid2

--- fit_grid.m
+++ fit_grid.m
@@ -1,14 +1,18 @@
 % explore grid of parameters
 
 nsess = 1000;
-S = logspace(-4, 4, 9);
-Q = logspace(-4, 4, 9);
+
+S = logspace(-3, 1, 9);
+S = S(2:end-1); % skip ends
+
+Q = logspace(-2, 0, 9);
+Q = Q(2:end-1); % skip ends
 
 for i = 1:length(S)
     for j = 1:length(Q)
         s = S(i);
         q = Q(j);
-        filename = sprintf('grid1_s=%.4f_q=%.4f_nsess=%d.mat', s, q, nsess);
+        filename = sprintf('grid2_s=%.6f_q=%.6f_nsess=%d.mat', s, q, nsess);
         disp(filename);
         tic
         [ex, agent] = run([s, q], @choose_Thompson, nsess);
